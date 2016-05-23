4e90fcef077df9561ebb8d4bee8db01aaa92139a
kpsec02 <kpsec02@RATHANA>
2016-05-23 15:24:04 +0700

    new class

---
+++ HwControl/src/Person.java
@@ -0,0 +1,24 @@
+
+public class Person {
+	String name, gen;
+	int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGen() {
+		return gen;
+	}
+	public void setGen(String gen) {
+		this.gen = gen;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
